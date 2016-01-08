6bee61e62d1c6c7eac2b6eae67566ea37fce70dc
Robin THONI <robin@rthoni.com>
2016-01-08 15:04:23 +0100

    gitignore; fixed for newer avr-gcc

--- UsbKeyboard.h
+++ UsbKeyboard.h
@@ -33,7 +33,7 @@ static uchar    idleRate;           // in 4 ms units
  * Redundant entries (such as LOGICAL_MINIMUM and USAGE_PAGE) have been omitted
  * for the second INPUT item.
  */
-PROGMEM char usbHidReportDescriptor[35] = { /* USB report descriptor */
+PROGMEM const char usbHidReportDescriptor[35] = { /* USB report descriptor */
   0x05, 0x01,                    // USAGE_PAGE (Generic Desktop) 
   0x09, 0x06,                    // USAGE (Keyboard) 
   0xa1, 0x01,                    // COLLECTION (Application) 
